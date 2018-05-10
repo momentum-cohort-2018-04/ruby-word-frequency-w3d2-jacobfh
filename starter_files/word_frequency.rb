4d0eeb7b293eb447aed7ef2f621890715cb7ed4f
Jacobfh <jfh708@gmail.com>
2018-05-10 08:18:11 -0400

    framework set for setting up methods

--- starter_files/word_frequency.rb
+++ starter_files/word_frequency.rb
@@ -4,18 +4,34 @@ class Wordfreq
     'were', 'will', 'with']
 
   def initialize(filename)
+    @filename = filename
   end
 
-  def frequency(word)
+#  - remove punctuation                    //[[:punct:]]//
+#  - normalize all words to lowercase        .downcase
+#  - remove "stop words" -- words used so frequently they are ignored      != STOP_WORDS
+#  - go through the file word by word and keep a count of how often each word is used
+
+# - assign word to the key (k) and number of frequencies to value (v)
+
+  def frequency(word) 
+    #run file
+      words = string.split(' ')
+      frequency = Hash.new(0)
+      words.each { |word| frequency[word.downcase] += 1 }
+      return frequency
   end
 
-  def frequencies
+  def frequencies  #count by calling freq
   end
 
-  def top_words(number)
+  def top_words(number) #sort by calling freqs
+    frequencies
+    sort -v
+    take(num)
   end
 
-  def print_report
+  def print_report  #make it look like example
   end
 end
 
